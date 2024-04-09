32df148734bd2a0a9d2a86dbea0d82f671342f21
Elmar Klausmeier <Elmar.Klausmeier@gmail.com>
2024-04-09 20:31:02 +0200

    Added rendering time in footer

--- templates/bottom-layout.php
+++ templates/bottom-layout.php
@@ -1,9 +1,11 @@
 
 	<footer>
 		<p><br><br><?php
-			printf("Generated %s CET (Europe/Berlin) using <a href=\"%s\">Simplified Saaze</a>%s<br><br>\n",
+			printf("Generated %s CET (Europe/Berlin) using <a href=\"%s\">Simplified Saaze</a>%s%s<br><br>\n",
 				date('d-M-y H:i'), '/blog/2021/10-31-simplified-saaze',
-				getenv('NON_NGINX') ? '' : ', served by <a href="https://nginx.org">NGINX</a>');
+				getenv('NON_NGINX') ? '' : ', served by <a href="https://nginx.org">NGINX</a>',
+				isset($_SERVER['REQUEST_TIME_FLOAT']) ? sprintf(", rendered in %.2f ms",1000 * (microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'])) : ''
+			);
 			?>
 		</p>
 	</footer>
